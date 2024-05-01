5bb5600c2ece2391e77571e3d632f4d3bdf9079b
quinnluk1 <149325965+quinnluk1@users.noreply.github.com>
2024-04-30 22:37:05 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,8 +10,12 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols)
   {
-    /* to be implemented in part (a) */
-
+    lights = new boolean [numRows[numCols];
+    for(int r=0; r<numRows;r++){
+      for(int c =0; c<numCols;c++){
+        lights[r][c]=Math.Random()<0.4;
+      }
+    }
   }
 
   /** Evaluates a light in row index row and column index col and returns a status
@@ -20,9 +24,16 @@ public class LightBoard
    */
   public boolean evaluateLight(int row, int col)
   {
-    /* to be implemented in part (b) */
-   
- 
+    int count = 0;
+    for(int i =0; i<lights.length;i++){
+      if(lights[row][i]==true)
+        count++;
+    }
+    if(lights[row][col]&&count%2==0)
+      return false;
+    if(!lights[row][col]&&count%3==0)
+      return true;
+    return lights[row][col];
   }
   public boolean[][] getLights()
   {
